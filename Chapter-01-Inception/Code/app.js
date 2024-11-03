96c48e8f9d37566c1bfd9ff9d39032141405ddad
Shibani Das <1610shibani@gmail.com>
2024-11-03 13:06:00 +0530

    Chapter-1 Added

--- Chapter-01-Inception/Code/app.js
+++ Chapter-01-Inception/Code/app.js
@@ -7,3 +7,34 @@ const headingElement = React.createElement(
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(headingElement);
+
+//  HOW TO CREATE THE FOLLOWING NESTED HTML STRUCTURE ?
+/**
+ * <div id="parent">
+ *      <div id="child">
+ *          <h1>H1 Tag of Child Div </h1>
+ *          <h2>H2 Tag of Child Div </h2>
+ *      </div>
+ *      <div id="child2">
+ *          <h1>H1 Tag of Child-2 Div </h1>
+ *          <h2>H2 Tag of Child-2 Div </h2>
+ *      </div>
+ * </div>
+ *  
+ */
+
+
+const parent = React.createElement(
+    "div", 
+    {id:"parent"}, 
+    [React.createElement("div", {id:"child"},
+        [React.createElement("h1",{},"H1 Tag of Child Div"),
+         React.createElement("h2",{},"H2 Tag of Child Div")] ), 
+    React.createElement("div", {id:"child2"},
+        [React.createElement("h1",{},"H1 Tag of Child-2 Div"),
+        React.createElement("h2",{},"H2 Tag of Child-2 Div")
+    ])]
+)
+
+root.render(parent); // This overwrites all the HTML content that was stated in the "root" section.
+
