b7c8c08f4f8dfd6518468f3899c2e2264caedf1d
Shibani Das <1610shibani@gmail.com>
2024-12-29 23:15:49 +0530

    Chapter -04 Added

--- Chapter-04-Show-the-Code/Code/App.js
+++ Chapter-04-Show-the-Code/Code/App.js
@@ -30,14 +30,20 @@ const styleCard = {
 };
 */
 
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
+    // props is a JavaScript Object
+    // as it is Javascript the props.key must be enclosed in "{}" {props.key}
+    console.log(props); 
+    // Destructing of props object :
+    // const {resName, cuisine, rating, delTime} = props
+
     return (
         <div className="res-card" style={{ backgroundColor : "#E6E9F0"}}>
             <img className="res-logo" alt="res-logo" src="https://www.freshtohome.com/blog/wp-content/uploads/2024/08/Biryani.jpeg" />
-            <h3>Meghna Foods</h3>
-            <h4>Biryani, Asian</h4>
-            <h4>4.4 Stars</h4>
-            <h4>30 mins</h4>
+            <h3>{props.resName}</h3>
+            <h4>{props.cuisine}</h4>
+            <h4>{props.rating}</h4>
+            <h4>{props.delTime}</h4>
         </div>
     )
 
@@ -48,14 +54,8 @@ const Body = () => {
         <div className="body">
             <div className="search">Search</div>
             <div className="res-container">
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
+                <RestaurantCard resName="Meghna Foods" cuisine="Biryani Asian" rating="4.4 Stars" delTime="30 Mins" />
+                <RestaurantCard resName="Andhra Gun Powder" cuisine="Biryani, South Indian" rating="4.2 Stars" delTime="40 Mins"/>
             </div>
         </div>
     )
