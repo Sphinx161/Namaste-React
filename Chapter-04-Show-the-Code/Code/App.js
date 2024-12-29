f875bc15475b419da930c99d55fcb35936edc7d4
Shibani Das <1610shibani@gmail.com>
2024-12-30 00:29:38 +0530

    Chapter-04 Updated

--- Chapter-04-Show-the-Code/Code/App.js
+++ Chapter-04-Show-the-Code/Code/App.js
@@ -30,20 +30,90 @@ const styleCard = {
 };
 */
 
+const resList = [
+    {
+        id: "1001",
+        name : "Santosh Family Dhaba",
+        receipe: "Tomato Soup",
+        rating: "4.0",
+        price : "₹300 for two",
+        deliveryTime: "22 MINS",
+        image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/mkcibe6x1ychvzjkywxk",
+    },
+    {
+        id: "1002",
+        name : "Cafe Niloufer",
+        receipe: "Maska Bun",
+        rating: "4.5",
+        price : "₹400 for two",
+        deliveryTime: "32 MINS",
+        image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/dc9t0kiq7lhsd3pndohf",
+    },
+    {
+        id: "1003",
+        name : "Santosh Family Dhaba",
+        receipe: "Crispy Corn",
+        rating: "4.5",
+        price : "₹560 for two",
+        deliveryTime: "32 MINS",
+        image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/dwqb8nhiop8fvqya0alq",
+    },
+    {
+        id: "1004",
+        name : "Grameen Kulfi",
+        receipe: "Sitaphal Stick Kulfi",
+        rating: "4.6",
+        price : "₹120 for two",
+        deliveryTime: "18 MINS",
+        image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/vu0thsifji9wgfuzfmic",
+    },
+    {
+        id: "1005",
+        name : "Nrs Nandini",
+        receipe: "Idli",
+        rating: "4.4",
+        price : "₹150 for two",
+        deliveryTime: "21 MINS",
+        image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/9fa5ca30c3db622c69cec0326ff63b41",
+    },
+    {
+        id: "1006",
+        name : "Utsav Vegetarian",
+        receipe: "Kaju Paneer Biryani",
+        rating: "4.1",
+        price : "₹300 for two",
+        deliveryTime: "32 MINS",
+        image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/y2lkcxxax3794vepe2se",
+    },
+    {
+        id: "1007",
+        name : "Subbayya Gari Hotel",
+        receipe: "Butta Bojanam",
+        rating: "4.0",
+        price : "₹300 for two",
+        deliveryTime: "24 MINS",
+        image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/iwtocxqjtu57vc7l52h9",
+    },
+]
+
+
 const RestaurantCard = (props) => {
     // props is a JavaScript Object
     // as it is Javascript the props.key must be enclosed in "{}" {props.key}
     console.log(props); 
     // Destructing of props object :
     // const {resName, cuisine, rating, delTime} = props
-
+    const {resInfo} = props;
+    const {name, receipe, rating, price, deliveryTime, image} = resInfo;
+    // console.log({resInfo.name}); // Not working ?
     return (
         <div className="res-card" style={{ backgroundColor : "#E6E9F0"}}>
-            <img className="res-logo" alt="res-logo" src="https://www.freshtohome.com/blog/wp-content/uploads/2024/08/Biryani.jpeg" />
-            <h3>{props.resName}</h3>
-            <h4>{props.cuisine}</h4>
-            <h4>{props.rating}</h4>
-            <h4>{props.delTime}</h4>
+            <img className="res-logo" alt="res-logo" src={image}/>
+            <h3>{name}</h3>
+            <h4>{receipe}</h4>
+            <h4>{rating}</h4>
+            <h4>{price}</h4>
+            <h4>{deliveryTime}</h4>
         </div>
     )
 
@@ -54,8 +124,10 @@ const Body = () => {
         <div className="body">
             <div className="search">Search</div>
             <div className="res-container">
-                <RestaurantCard resName="Meghna Foods" cuisine="Biryani Asian" rating="4.4 Stars" delTime="30 Mins" />
-                <RestaurantCard resName="Andhra Gun Powder" cuisine="Biryani, South Indian" rating="4.2 Stars" delTime="40 Mins"/>
+                {
+                    resList.map(restaurant => 
+                        (<RestaurantCard key={restaurant.id} resInfo={restaurant} />))
+                }
             </div>
         </div>
     )
